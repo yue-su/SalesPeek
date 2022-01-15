eeeb88e1aa0dcffe2fac7b55e3fb7c482a60ee9f
Yue Su <su2047@gmail.com>
2022-01-15 14:14:40 -0400

    project setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '434520797b3b29423298b13ef3be7ca4'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b23c328c-406e-4cb1-877c-59b8bcdd06d0'
+};
